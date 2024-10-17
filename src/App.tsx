8606af18073409e9946a2d29f66fffd97c467a7e
pablobg9 <128375474+pablobg9@users.noreply.github.com>
2024-10-17 09:42:28 +0200

    1ªVersionPokemonCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from "react"
-import CameraCard from './components/CameraCard/CameraCard'
+import React from "react";
+import CameraCard from "./components/CameraCard/CameraCard";
 
 function App() {
-
   return (
-    <div>
-      <CameraCard />
-      <CameraCard />
-      <CameraCard />
+    <div className="h-screen bg-[#18a1ad]">
+      <div className="absolute top-4 left-4 w-96">
+        <img
+          className="flex items-center justify-center w-full h-auto max-w-none"
+          src="./src/assets/images/bg-pattern-top.svg"
+        />
+      </div>
+
+      <div className="flex items-center justify-center h-full relative z-10">
+        <CameraCard />
+      </div>
+
+      <div className="flex items-center justify-center absolute bottom-4 right-4 w-">
+        <img
+          className="w-full h-auto max-w-none"
+          src="./src/assets/images/bg-pattern-bottom.svg"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CameraCard/CameraCard.tsx
+++ src/components/CameraCard/CameraCard.tsx
@@ -1,76 +1,81 @@
 import { useEffect, useState } from "react";
 
 interface newPokemon {
-  id: number,
-  sprites: string,
-  name: string,
-  base_experience: number,
-  hp: number,
-  attack: number,
-  specialatt: number,
-  deffense: number
- } 
+  id: number;
+  sprites: string;
+  name: string;
+  base_experience: number;
+  hp: number;
+  attack: number;
+  specialatt: number;
+  deffense: number;
+}
 
-const x = Math.ceil(Math.random()*150);
-console.log(x);
-
-
-const POKEMON_RANDOM_ID = `https://pokeapi.co/api/v2/pokemon/${x}`
-console.log(POKEMON_RANDOM_ID);
+const x = Math.ceil(Math.random() * 151);
+const POKEMON_RANDOM_ID = `https://pokeapi.co/api/v2/pokemon/${x}`;
 
 export default function CameraCard() {
-
-  const[pokemon, setPokemon] = useState <newPokemon> ()
+  const [pokemon, setPokemon] = useState<newPokemon | null>(null);
 
   useEffect(() => {
     fetch(POKEMON_RANDOM_ID)
-      .then(res=>res.json())
-      .then(data=>{
-        const { id, sprites, name, base_experience, stats } = data
+      .then((res) => res.json())
+      .then((data) => {
+        const { id, sprites, name, base_experience, stats } = data;
         const newPokemon = {
           id: id,
-          sprites: sprites.front_shiny,
+          sprites: sprites.other.dream_world.front_default,
           name: name,
           base_experience: base_experience,
           hp: stats[0].base_stat,
           attack: stats[1].base_stat,
           specialatt: stats[3].base_stat,
-          deffense: stats[2].base_stat
-        }
-        setPokemon(newPokemon)
-      })
-  }, [])
+          deffense: stats[2].base_stat,
+        };
+        setPokemon(newPokemon);
+      });
+  }, []);
 
+  return (
+    <div className=" flex flex-col items-center justify-between rounded-2xl shadow-md w-82 h-104 overflow-hidden bg-white">
+      <header className="w-full">
+        <img
+          className="w-full h-32"
+          src="./src/assets/images/bg-pattern-card.svg"
+        />
+      </header>
 
-  
-    return (
-      
-      <div className="flex items-center justify-center h-screen">
-        <div className="flex flex-col items-center justify-between border rounded-lg shadow-md p-6 w-80 h-96">
-          <header className="mb-4 	z-index: 1;">
-          <img
-             src="./src/assets/images/bg-pattern-card.svg"
-           />
-          </header>
-          <div className="text-center mb-4">
-          <img
-             className="w-32 h-32 rounded-full border-2 border-gray-300 mt-2 	z-index: 1;"
-             src={pokemon?.sprites}
-           />
-            <div className="flex justify-between items-center w-full">
-              <h1 className="font-bold">{pokemon?.name}</h1>
-              <p className="text-gray-600">{pokemon?.hp}hp</p>
-            </div>
-            <p className="text-gray-600 mt-2">{pokemon?.base_experience} exp</p>
-          </div>
-          <hr className="my-4 border-black" />
-          <hr className="border-black w-full" /> 
-          <footer className="flex justify-between w-full mt-4">
-            <h3 className="text-lg text-center">{pokemon?.attack} Ataque</h3>
-            <h3 className="text-lg text-center">{pokemon?.specialatt} AtaqueEspecial</h3>
-            <h3 className="text-lg text-center">{pokemon?.deffense} Defensa</h3>
-          </footer>
-        </div>
+      <div className=" -mt-28">
+        <img
+          className="w-52 h-52 rounded-full bg-white border-4 border-white mx-auto"
+          src={pokemon?.sprites}
+        />
       </div>
-    );
-  }
+
+      <div className="text-center mt-3">
+        <div className="flex justify-between items-center w-full ">
+          <h1 className="font-bold capitalize">{pokemon?.name}</h1>
+          <p className="text-gray-400 mx-1">{pokemon?.hp}hp</p>
+        </div>
+        <p className="text-gray-400 mt-2">{pokemon?.base_experience} exp</p>
+      </div>
+
+      <hr className="my-7 border-gray-300 w-full" />
+
+      <footer className="flex justify-between w-full px-6 mb-6">
+        <div className="text-center mx-2">
+          <h3 className="text-xs font-black mb-3">{pokemon?.attack}K</h3>
+          <p className="text-xs">Ataque</p>
+        </div>
+        <div className="text-center mx-2">
+          <h3 className="text-xs font-black mb-3">{pokemon?.specialatt}K</h3>
+          <p className="text-xs ">Ataque Especial</p>
+        </div>
+        <div className="text-center mx-2">
+          <h3 className="text-xs font-black mb-3">{pokemon?.deffense}K</h3>
+          <p className="text-xs">Defensa</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
